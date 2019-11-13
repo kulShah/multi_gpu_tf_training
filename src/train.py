c8ab0a94cf7425e507a6c6bae0e248263ba3925e
Kuldeep Shah <kul.n.shah@gmail.com>
2019-11-13 15:39:40 -0800

    bugfix: train.py had indentation errors, changed num_steps in config

--- src/train.py
+++ src/train.py
@@ -35,69 +35,63 @@ with tf.device('/cpu:0'):
   for i in range(num_gpus):
     with tf.device(assign_to_device('/gpu:{}'.format(i), ps_device='/cpu:0')):
 
-      # split data between GPUs
+      # Split data between GPUs
       _x = X[i * batch_size : (i+1) * batch_size]
       _y = Y[i * batch_size : (i+1) * batch_size]
 
-      # Since dropout has different behavior at training and prediction time,
-      # we need to create 2 distinct computation graphs that share the same weights
+      # Because Dropout have different behavior at training and prediction time, we
+      # need to create 2 distinct computation graphs that share the same weights.
 
-      # create a graph for training
+      # Create a graph for training
       logits_train = conv_net(_x, num_classes, dropout, reuse=reuse_vars, is_training=True)
-
-      # create a graph for testing that resuse the same weights
+      # Create another graph for testing that reuse the same weights
       logits_test = conv_net(_x, num_classes, dropout, reuse=True, is_training=False)
 
-      # define loss and optimizer (with train logits, for dropout to take effect)
+      # Define loss and optimizer (with train logits, for dropout to take effect)
       loss_op = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=logits_train, labels=_y))
       optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
       grads = optimizer.compute_gradients(loss_op)
 
-      # only first GPU compute accuracy
+      # Only first GPU compute accuracy
       if i == 0:
-        # evaluate model (with test logits, for dropout to be disabled)
-        correct_pred = tf.equal(tf.argmax(logits_test, 1), tf.argmax(_y, 1))
-        accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+          # Evaluate model (with test logits, for dropout to be disabled)
+          correct_pred = tf.equal(tf.argmax(logits_test, 1), tf.argmax(_y, 1))
+          accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
       reuse_vars = True
       tower_grads.append(grads)
 
-    tower_grads = average_gradients(tower_grads)
-    train_op = optimizer.apply_gradients(tower_grads)
+  tower_grads = average_gradients(tower_grads)
+  train_op = optimizer.apply_gradients(tower_grads)
 
-    # initializing the variables
-    init = tf.global_variables_initializer()
+  # Initialize the variables (i.e. assign their default value)
+  init = tf.global_variables_initializer()
 
-    # launch the graph
-    with tf.Session() as sess:
+  # Start Training
+  with tf.Session() as sess:
 
-      # run the initializer
-      sess.run(init)
+    # Run the initializer
+    sess.run(init)
 
-      # step = 1
+    # Keep training until reach max iterations
+    for step in range(1, num_steps + 1):
+      # Get a batch for each GPU
+      batch_x, batch_y = mnist.train.next_batch(batch_size * num_gpus)
+      # Run optimization op (backprop)
+      ts = time.time()
+      sess.run(train_op, feed_dict={X: batch_x, Y: batch_y})
+      te = time.time() - ts
+      if step % display_step == 0 or step == 1:
+          # Calculate batch loss and accuracy
+          loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x, Y: batch_y})
+          print("Step " + str(step) + \
+                ": Minibatch Loss= " + "{:.4f}".format(loss) + \
+                 ", Training Accuracy= " + "{:.3f}".format(acc) + \
+                 ", %i Examples/sec" % int(len(batch_x)/te) )
+        
+    print("Optimization Finished!")
 
-      # keep training until reach max iterations:
-      for step in range(1, num_steps + 1):
-        # get a batch for each GPU
-        batch_x, batch_y = mnist.train.next_batch(batch_size * num_gpus)
-
-        # run optimization op (backprop)
-        ts = time.time()
-        sess.run(train_op, feed_dict={X:batch_x, Y:batch_y})
-        te = time.time() - ts
-
-        if step % display_step == 0 or step == 1:
-          # calculate batch loss and accuracy
-          loss, acc = sess.run([loss_op, accuracy], feed_dict={X: batch_x, Y:batch_y})
-          print( "step = " + str(step) + \
-                " minibatch loss = " + "{:.4f}".format(loss) + \
-                " training accuracy = " + "{:.3f}".format(acc) + \
-                " , %i examples/sec" % int(len(batch_x)/ te) )
-
-        # step += 1
-      print("optimization finished")
-
-      # calculate accuracy for 1000 mnist test images
-      print("testing accuracy = ", \
-            np.mean( [ sess.run( accuracy, feed_dict={X: mnist.test.images[i:i+batch_size], Y: mnist.test.labels[i:i+batch_size] } ) \
-                       for i in range(0, len(mnist.test.images), batch_size ) ] ) )
+    # Calculate accuracy for MNIST test images
+    print( "Testing Accuracy:", \
+      np.mean( [ sess.run( accuracy, feed_dict={X: mnist.test.images[i:i+batch_size], Y: mnist.test.labels[i:i+batch_size] } ) \
+                  for i in range(0, len(mnist.test.images), batch_size) ] ) )
